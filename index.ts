dc19a9cf033683807e68680a338e0303612d2c6c
PRoy <sam469roger@gmail.com>
2024-05-27 12:29:08 +0530

    setup admin and vendor route

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import { AdminRoute, VendorRoute } from './routes';
+
 const app = express();
 
-app.use('/', (req, res) => {
-  return res.json('Hello World!');
-});
+app.use('/admin', AdminRoute);
+app.use('/vendor', VendorRoute);
 
 app.listen(8000, () => {
   console.log('App listening on port 8000');
